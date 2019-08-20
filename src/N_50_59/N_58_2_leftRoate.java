fdcf904a1b61141a032083ba1528c78da8ccf71c
fangyinrui <fangyinrui@163.com>
2019-08-20 20:38:37 +0800

    N_58(2):循环左移

---
+++ src/N_50_59/N_58_2_leftRoate.java
@@ -0,0 +1,33 @@
+package N_50_59;
+/**
+ * ×óĞı×ª×Ö·û´®£¨Ñ­»·×óÒÆ£©
+ *
+ */
+public class N_58_2_leftRoate {
+
+	public static String leftRotate(String str,int k) {
+		char[] cs = str.toCharArray();
+		reverse(cs,0,k-1);
+		reverse(cs,k,cs.length-1);
+		reverse(cs,0,cs.length-1);
+		
+		return String.valueOf(cs);
+	}
+	
+	public static void reverse(char[] cs , int i , int j) {
+		while(i<=j) {
+			char t = cs[i];
+			cs[i] = cs[j];
+			cs[j] = t;
+			i++;
+			j--;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "abcXYZ";
+		System.out.println(leftRotate(str,3));
+	}
+
+}
